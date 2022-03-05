ed760a0b2962e625ccb7d8e726201a89eab1062f
jack <jack9robinson@gmail.com>
2022-03-05 18:54:10 -0500

    first commit

---
+++ comp3097-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  comp3097-project
+//
+//  Created by Jack Robinson on 2022-03-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
